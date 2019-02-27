114ca6f71e447e6025f22ea18d5cb20a2b362ae8
Zafar Saleem <zafarsaleem3@gmail.com>
2019-02-27 18:16:57 -0500

    TASK: Admin check sent from client

--- server/utils/admin.js
+++ server/utils/admin.js
@@ -1,16 +1,28 @@
 const User = require('../models/User');
 
-function isAdmin(id) {
+function isAdmin(id, role) {
   return new Promise((resolve, reject) => {
-    User.findOne({ _id: id }, (error, doc) => {
-      console.log(doc);
-      console.log(error)
-      if (error) return reject(false);
-      if (doc.role.toLowerCase() === 'admin') {
-        console.log('TRUE')
-        return resolve(true);
-      }
-    });
+    checkClientSentControl(role)
+      .then(() => {
+        User.findOne({ _id: id }, (error, doc) => {
+          if (error) return reject(false);
+          if (doc.role.toLowerCase() === 'admin') {
+            return resolve(true);
+          }
+        });
+      })
+      .catch(error => {
+        return reject(false);
+      });
+  });
+}
+
+function checkClientSentControl(role) {
+  return new Promise((resolve, reject) => {
+    if (role.toLowerCase() !== 'admin') {
+      return reject(false);
+    }
+    return resolve(true);
   });
 }
 
