9366805c4a11739352cd9ef1e14a69c94bbd1df1
Zafar Saleem <zafarsaleem3@gmail.com>
2019-02-27 11:49:05 -0500

    TASK: Check user control from client

--- server/services/admin/new.js
+++ server/services/admin/new.js
@@ -1,6 +1,13 @@
 const User = require('../../models/User');
 
 function newEmployee(request, response) {
+  if (request.body.access.role.toLowerCase() !== 'admin') {
+    return response.json({
+      success: false,
+      message: 'Only admin can perform this action.'
+    });
+  }
+  
   new User(request.body)
     .save((error, doc) => {
       if (error) return response.json({ success: false, message: error.message });
