240e9eb76c06f170b0cdfa20b3b25bd0119ab4a7
Zafar Saleem <zafarsaleem3@gmail.com>
2019-02-27 13:21:30 -0500

    TASK: Check admin on server

--- server/services/admin/new.js
+++ server/services/admin/new.js
@@ -6,14 +6,27 @@ function newEmployee(request, response) {
   if (request.body.access.role.toLowerCase() !== 'admin') {
     return response.json(httpResponses.onClientAdminFail);
   }
-  
-  new User(request.body)
-    .save((error, doc) => {
-      if (error) return response.json({ success: false, message: error.message });
-      response.json({
-        success: true,
-        message: `${request.body.name} created successfully.`
-      });
+
+  utils.isAdmin(request.body.access.id)
+    .then(admin => {
+      new User(request.body.data)
+        .save((error, doc) => {
+          console.log(error);
+          console.log(doc)
+          if (error) {
+            return response.json({
+              success: false,
+              message: error.errmsg
+            });
+          }
+          return response.json({
+            success: true,
+            message: `${request.body.data.name} created successfully.`
+          });
+        });
+    })
+    .catch(err => {
+      return response.json(httpResponses.onServerAdminFail);
     });
 }
 
